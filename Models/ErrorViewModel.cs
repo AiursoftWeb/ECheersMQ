5c487dd5a72024a7638792f3c41074e73c7b03e0
Anduin2017 <Anduin@aiursoft.com>
2018-01-22 19:00:22 +0800

    init project.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mq.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
